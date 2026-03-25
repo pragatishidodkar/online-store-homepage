fddd72678b29746139a8c940910fb1eac57f36cd
pragatishidodkar <pragatishidodkar@gmail.com>
2026-03-25 23:17:26 +0530

    Add product data to products-data.js
    Added an array of product objects with details including name, category, price, rating, and image URL.

--- products-data.js
+++ products-data.js
@@ -1,1 +1,51 @@
+const products = [
 
+{
+name:"Wireless Headphones",
+category:"Electronics",
+price:1999,
+rating:4,
+image:"https://via.placeholder.com/200"
+},
+
+{
+name:"Smart Watch",
+category:"Electronics",
+price:2999,
+rating:5,
+image:"https://via.placeholder.com/200"
+},
+
+{
+name:"Men T-Shirt",
+category:"Fashion",
+price:499,
+rating:4,
+image:"https://via.placeholder.com/200"
+},
+
+{
+name:"Jeans",
+category:"Fashion",
+price:1299,
+rating:3,
+image:"https://via.placeholder.com/200"
+},
+
+{
+name:"Cooking Pan",
+category:"Home",
+price:899,
+rating:4,
+image:"https://via.placeholder.com/200"
+},
+
+{
+name:"Novel Book",
+category:"Books",
+price:299,
+rating:5,
+image:"https://via.placeholder.com/200"
+}
+
+];
