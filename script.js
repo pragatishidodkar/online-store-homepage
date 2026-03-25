eb297e5992d3449281286ec6c15e9fad0c680e7b
pragatishidodkar <pragatishidodkar@gmail.com>
2026-03-25 23:21:36 +0530

    Implement product display functionality
    Added a function to display products dynamically in the product container.

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+const container = document.getElementById("productContainer");
 
+function displayProducts(list){
+
+container.innerHTML = "";
+
+list.forEach(product => {
+
+const card = document.createElement("div");
+
+card.classList.add("product-card");
+
+card.innerHTML = `
+
+<img src="${product.image}">
+
+<h3>${product.name}</h3>
+
+<p>₹${product.price}</p>
+
+<p class="rating">${"⭐".repeat(product.rating)}</p>
+
+<button>Add to Cart</button>
+
+`;
+
+container.appendChild(card);
+
+});
+
+}
+
+displayProducts(products);
