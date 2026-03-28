c3f5eb77f73340183b952aedd880262c2b1bd481
pragatishidodkar <pragatishidodkar@gmail.com>
2026-03-28 21:22:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
 const container = document.getElementById("productContainer");
 
-let allProducts = [];
-
 let currentCategory = "All";
 let currentSearch = "";
 let currentSort = "";
 let currentPrice = "";
 let currentRating = "";
 
-// Fetch products
-fetch("https://fakestoreapi.com/products")
-.then(res => res.json())
-.then(data => {
-allProducts = data;
-applyFilters();
-});
 
-// Display
 function displayProducts(list){
 
 container.innerHTML = "";
@@ -29,9 +19,9 @@ card.classList.add("product-card");
 
 card.innerHTML = `
 <img src="${product.image}">
-<h3>${product.title}</h3>
-<p>$${product.price}</p>
-<p class="rating">${"⭐".repeat(Math.round(product.rating.rate))}</p>
+<h3>${product.name}</h3>
+<p>₹${product.price}</p>
+<p class="rating">${"⭐".repeat(product.rating)}</p>
 <button>Add to Cart</button>
 `;
 
@@ -42,10 +32,9 @@ container.appendChild(card);
 }
 
 
-// Main filter system
 function applyFilters(){
 
-let result = [...allProducts];
+let result = [...products];
 
 // Category
 if(currentCategory !== "All"){
@@ -55,8 +44,24 @@ result = result.filter(p => p.category === currentCategory);
 // Search
 if(currentSearch){
 result = result.filter(p =>
-p.title.toLowerCase().includes(currentSearch)
+p.name.toLowerCase().includes(currentSearch)
 );
+}
+
+// Price
+if(currentPrice === "low"){
+result = result.filter(p => p.price < 500);
+}
+else if(currentPrice === "mid"){
+result = result.filter(p => p.price >= 500 && p.price <= 2000);
+}
+else if(currentPrice === "high"){
+result = result.filter(p => p.price > 2000);
+}
+
+// Rating
+if(currentRating){
+result = result.filter(p => p.rating >= currentRating);
 }
 
 // Sorting
@@ -67,7 +72,7 @@ else if(currentSort === "high"){
 result.sort((a,b)=>b.price - a.price);
 }
 else if(currentSort === "rating"){
-result.sort((a,b)=>b.rating.rate - a.rating.rate);
+result.sort((a,b)=>b.rating - a.rating);
 }
 
 displayProducts(result);
@@ -81,16 +86,30 @@ currentCategory = category;
 applyFilters();
 }
 
-
 // Search
 document.getElementById("search").addEventListener("input",(e)=>{
 currentSearch = e.target.value.toLowerCase();
 applyFilters();
 });
 
-
 // Sorting
 function sortProducts(type){
 currentSort = type;
 applyFilters();
 }
+
+// Price filter
+function filterPrice(type){
+currentPrice = type;
+applyFilters();
+}
+
+// Rating filter
+function filterRating(value){
+currentRating = Number(value);
+applyFilters();
+}
+
+
+// Initial load
+applyFilters();
