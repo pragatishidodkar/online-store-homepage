d5ae526e2937837a8f6536b52ee7792bd3502c84
pragatishidodkar <pragatishidodkar@gmail.com>
2026-03-28 21:43:20 +0530

    Update products-data.js

--- products-data.js
+++ products-data.js
@@ -1,51 +1,43 @@
 const products = [
 
 {
-name:"Wireless Headphones",
-category:"Electronics",
-price:1999,
-rating:4,
-image:"https://via.placeholder.com/200"
+name: "WHITE HANGING HEART T-LIGHT HOLDER",
+price: 2.55,
+category: "Home",
+rating: 4,
+image: "https://images.unsplash.com/photo-1582719478250-c89cae4dc85b"
 },
 
 {
-name:"Smart Watch",
-category:"Electronics",
-price:2999,
-rating:5,
-image:"https://via.placeholder.com/200"
+name: "WHITE METAL LANTERN",
+price: 3.39,
+category: "Home",
+rating: 5,
+image: "https://images.unsplash.com/photo-1505693416388-ac5ce068fe85"
 },
 
 {
-name:"Men T-Shirt",
-category:"Fashion",
-price:499,
-rating:4,
-image:"https://via.placeholder.com/200"
+name: "CREAM CUPID HEARTS COAT HANGER",
+price: 2.75,
+category: "Home",
+rating: 4,
+image: "https://images.unsplash.com/photo-1519710164239-da123dc03ef4"
 },
 
 {
-name:"Jeans",
-category:"Fashion",
-price:1299,
-rating:3,
-image:"https://via.placeholder.com/200"
+name: "KNITTED UNION FLAG HOT WATER BOTTLE",
+price: 3.39,
+category: "Fashion",
+rating: 3,
+image: "https://images.unsplash.com/photo-1523381210434-271e8be1f52b"
 },
 
 {
-name:"Cooking Pan",
-category:"Home",
-price:899,
-rating:4,
-image:"https://via.placeholder.com/200"
-},
-
-{
-name:"Novel Book",
-category:"Books",
-price:299,
-rating:5,
-image:"https://via.placeholder.com/200"
+name: "RED WOOLLY HOTTIE WHITE HEART",
+price: 3.39,
+category: "Fashion",
+rating: 4,
+image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff"
 }
 
 ];
